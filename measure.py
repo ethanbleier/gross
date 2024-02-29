47f839c57e3a9fa5f426ba92728d714d8826489d
ethan bleier <ebleier4@gmail.com>
2024-02-29 12:00:20 -0800

    added a clock and other stuff too

--- measure.py
+++ measure.py
@@ -1,4 +1,16 @@
-# This program aims to determine if research can be done with search
+#!/usr/bin/env python3
+
+''' 
+Ethan Bleier
+Inspired by: https://video.cs50.io/WbzNRTTrX0g?screen=0Towr-pBuzw&start=3969
+This program aims to determine if research can be done with search
+Code is adapted from a maze search algorithm
+
+For readability and simplicity, I'm beginning this project by comparing just two algorithms
+1: Bubble Sort 2: TBA, over one dataset, and graphing the results
+The graph should make it exeedingly clear which algo is faster
+data will become interesting as I add more algorithms
+'''
 
 import sys
 import csv
@@ -7,11 +19,25 @@ import csv
 class Node():
     def __init__(self, A, state, parent, dataset):
         self.A = A
+
+        # state should return the name of csv file? 
         self.state = state
+
+        # in case we want to go backwards for whatever future reason, 
+        # lets monitor the parent node
         self.parent = parent
+
+        # am I doing this right? 
         self.dataset = dataset
 
-# simple stack
+        # am I doing this right?
+        self.time = A.bubbleSort(dataset)
+        
+        # Switch to true after we measure the time
+        # am I doing this right?
+        self.measured = False
+
+# simple stack with a contains_dataset() -> bool
 class StackRoot():
     def __init__(self):
         self.root = []
@@ -45,73 +71,49 @@ class QueueRoot(StackRoot): # QueueRoot extends StackRoot (python > java)
             return node
 
 
-''' 
-Inspired by: https://video.cs50.io/WbzNRTTrX0g?screen=0Towr-pBuzw&start=3969
-'''
-
 class Measure():
 
     def __init__(self, filename):
+        # Need a way of keeping track of the best performing algorithm
         self.data = []
         with open(filename, 'r') as file:
             csv_file = csv.DictReader(file)
             for row in csv_file:
                 self.data.append(row)
 
-    def neighbors(self, state):
-        row, col = state
-        candidates = [
-            ("up", (row - 1, col)),
-            ("down", (row + 1, col)),
-            ("left", (row, col - 1)),
-            ("right", (row, col + 1))
-        ]
-
-        # result = []
-        # for action, (r, c) in candidates:
-        #     if 0 <= r < self.height and 0 <= c < self.width and not self.walls[r][c]:
-        #         result.append((action, (r, c)))
-        # return result
-
     def solve(self):
         """Finds best algorithm"""
+        from bubble_sort import BubbleSort
+        A = BubbleSort()
 
         # Keep track of number of states measured (instead of explored)
         self.num_measured = 0
 
-        # Initialize frontier to just the starting position
-        start = Node(state=self.start, parent=None, action=None)
+        # __init__(A, state, parent, dataset)
+        start = Node(state=self.start, A=A, parent=None, dataset=self.data)
         root = StackRoot()
         root.add(start)
 
         # Initialize an empty measured set
         self.measured = set()
 
-        # Keep looping until solution found
+        # Keep looping until optimal algorithm (A*) is found
         while True:
 
-            # If nothing left in frontier, then no path
+            # nothing left in root bruh
             if root.empty():
-                raise Exception("no solution")
+                raise Exception("oops")
 
-            # Choose a node from the frontier
+            # Choose a node from the root
             node = root.remove()
-            self.num_explored += 1
 
-            # If node is the goal, then we have a solution
-            if node.state == self.goal:
-                actions = []
-                cells = []
-                while node.parent is not None:
-                    actions.append(node.action)
-                    cells.append(node.state)
-                    node = node.parent
-                actions.reverse()
-                cells.reverse()
-                self.solution = (actions, cells)
-                return
-
-            # Mark node as explored
+            # probably wise to keep track of this
+            # self.num_explored += 1
+            
+            # measure the time taken to sort
+            # TODO: more thinking less random keystrokes
+            performance = A.bubbleSort(self.data) # big yikes here
+            node.measured = True
             self.explored.add(node.state)
 
             # Add neighbors to frontier
