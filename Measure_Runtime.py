c398a1adfef12f35c5250af52b62a2792ac8b909
ethan bleier <ebleier4@gmail.com>
2024-03-07 13:14:27 -0800

    updated bubble_sort.py

--- Measure_Runtime.py
+++ Measure_Runtime.py
@@ -5,15 +5,23 @@ import os
 import pprint
 import time
 from bubble_sort import BubbleSort
+import tqdm
+import matplotlib.pyplot as plt
 
 class Measure_Runtime():
 	def __init__(self, filename):
+		self.filename = filename
+		self.data = None
+
 		# if filename doesn't exist, means we probably havent generated the data yet. so we do that here.
-		if filename is None:
+		if filename is None or not os.path.exists(filename):
 				self.data = self.generate_data()
 		else:
 				self.data = self.read_data(filename)
 
+	def get_data(self):
+		return self.data
+	
 	# this method uses the DataGen.py script to...  ...generate data
 	def generate_data(self) -> list:
 		if os.path.exists("DataGen.py"):
@@ -38,19 +46,33 @@ class Measure_Runtime():
 					print(f"Encountered an invalid row({row})")
 		return data
 
-	@staticmethod
-	def solve(data):
+	def plot_runtime(runtimes):
+		arr_size = range(len(runtimes))
+		plt.plot(arr_size, runtimes, marker='o')
+		plt.xlabel('Array Length')
+		plt.ylabel('Runtime (s)')
+		plt.title('Bubble Sort Runtime Analysis')
+		plt.grid(True)
+		plt.show()
+
+	def solve(self, data):
+		# TODO: Refactor this
 		A = BubbleSort()
-		measured_count = set()
-		start = time.time()
-		A.bubbleSort(data)
-		end = time.time()
-		print(f"dataset size: {len(data)}, performance in seconds: {end - start:.6f}")
-		measured_count.add(1)
-		print("Measure count: ", measured_count)
+		new_data, runtimes = A.bubbleSort(data)
+		
+		print("**** DONE ****")
+		for item in range(len(new_data)):
+			pprint.pprint(new_data[item])
+		self.plot_runtime(runtimes)
+
 
 if __name__ == "__main__":
 	filename = "dataset/sawtoothAscendingDataWithNoise0.csv"
-	measure = Measure_Runtime(filename)
-	data = Measure_Runtime.solve(measure.data)
-	pprint.pprint(data)
+	A = Measure_Runtime(filename)
+	A_data = A.get_data()
+	
+	print("**** <START DATA> ****\n")
+	pprint.pprint(A_data)
+	print("**** <STOP DATA> ****\n\n")
+	A.solve(A_data)
+
--- bubble_sort.py
+++ bubble_sort.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # Source: https://www.geeksforgeeks.org/python-program-for-bubble-sort/
 # Classic bubble sort
+import tqdm
+import matplotlib.pyplot as plt
+import time
 
 class BubbleSort:
     @staticmethod
@@ -8,10 +11,25 @@ class BubbleSort:
         arr[i], arr[j] = arr[j], arr[i]
 
     @staticmethod
-    def bubbleSort(arr):
+    def bubbleSort(arr) -> list:
         # Main algorithm
         n = len(arr)
-        for i in range(n-1):
+        start = time.time()
+        for i in tqdm.tqdm(range(n-1), desc='Sorting', position=0, leave=True): 
             for j in range(0, n-i-1):
                 if arr[j] > arr[j + 1]:
                     BubbleSort.swap(arr, j, j+1)
+        end = time.time()
+        return arr, (end - start)
+
+    @staticmethod
+    def analyze_runtime():
+        array_lengths = list(range(100, 1001, 100))
+        runtimes = []
+
+        for length in array_lengths:
+            array = list(range(length, 0, -1))  # Reverse sorted array
+            sorted_array, runtime = BubbleSort.bubbleSort(array)
+            runtimes.append(runtime)
+
+        BubbleSort.plot_runtime(array_lengths, runtimes)
